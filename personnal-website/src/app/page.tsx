1d43bccda6a7adb685de6d68cc530915f1f6683a
vo_o <vo_o@etna-alternance.net>
2024-02-22 21:28:46 +0100

    [ADD => refactoring code

--- personnal-website/src/app/page.tsx
+++ personnal-website/src/app/page.tsx
@@ -1,37 +1,13 @@
 import { Parallax } from "./components/Parallax";
 import { MacbookScroll } from "./components/MacBoockScroll";
 import { FloatingNav } from "./components/FloatingBar";
+import { PRODUCTS } from "@/lib/utils";
 
 export default function Home() {
   return (
     <>
-      <Parallax
-        products={[
-          {
-            title: "Moissan Fighter",
-            link: "https://drive.google.com/file/d/1pCu_qf83LbPwMSwA57ww89hpfxn9TyKJ/view?usp=sharing",
-            thumbnail: "/assets/mfZ.png",
-          },
-          {
-            title: "Python Financial DashBoard",
-            link: "https://github.com/vo-olivier250405/pythonFinancialDashboard",
-            thumbnail: "/assets/pythonFinance.png",
-          },
-          {
-            title: "Gamify-tasks App",
-            link: "",
-            thumbnail: "/assets/gamifyTasks.png",
-          },
-          {
-            title: "Web-Organization App",
-            link: "",
-            thumbnail: "/assets/webapp.png",
-          },
-        ]}
-      />
-      <section className="h-screen">
-        <MacbookScroll title="Scroll to contact me" />
-      </section>
+      <Parallax products={PRODUCTS} />
+      <MacbookScroll title="Scroll to contact me" />
       <div></div>
     </>
   );
--- personnal-website/src/lib/utils.ts
+++ personnal-website/src/lib/utils.ts
@@ -4,3 +4,26 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const PRODUCTS = [
+  {
+    title: "Moissan Fighter",
+    link: "https://drive.google.com/file/d/1pCu_qf83LbPwMSwA57ww89hpfxn9TyKJ/view?usp=sharing",
+    thumbnail: "/assets/mfZ.png",
+  },
+  {
+    title: "Python Financial DashBoard",
+    link: "https://github.com/vo-olivier250405/pythonFinancialDashboard",
+    thumbnail: "/assets/pythonFinance.png",
+  },
+  {
+    title: "Gamify-tasks App",
+    link: "",
+    thumbnail: "/assets/gamifyTasks.png",
+  },
+  {
+    title: "Web-Organization App",
+    link: "",
+    thumbnail: "/assets/webapp.png",
+  },
+];
