2f2d455eb991b10406ca2471c2113ec1c5517a0f
vo_o <vo_o@etna-alternance.net>
2024-02-23 15:21:07 +0100

    [ADD => adding style for the contact me section

--- personnal-website/src/app/components/Links.tsx
+++ personnal-website/src/app/components/Links.tsx
@@ -1,47 +1,37 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faGithub,
-  faLinkedin,
-  faInstagram,
-} from "@fortawesome/free-brands-svg-icons";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { faFile, faMailBulk } from "@fortawesome/free-solid-svg-icons";
 
 export const Links = () => {
   return (
     <div className="mt-8">
       <a
-        className="m-4 p-4 border-2 rounded-md  text-white text-2xl"
+        className="m-4 p-4 border-2 rounded-md  text-white text-2xl hover:bg-gray-400 transition-all 0.3s ease-in hover:text-green-900"
         href="https://github.com/vo-olivier250405"
         target="blank"
       >
         <FontAwesomeIcon icon={faGithub} />
       </a>
       <a
-        className="m-4 p-4 border-2 rounded-md  text-white text-2xl"
+        className="m-4 p-4 border-2 rounded-md  text-white text-2xl hover:bg-gray-400 transition-all 0.3s ease-in hover:text-green-900"
         href="https://www.linkedin.com/in/voolivier/"
         target="blank"
       >
         <FontAwesomeIcon icon={faLinkedin} />
       </a>
+
       <a
-        className="m-4 p-4 border-2 rounded-md  text-white text-2xl "
-        href="https://www.instagram.com/volivieet/"
-        target="blank"
-      >
-        <FontAwesomeIcon icon={faInstagram} />
-      </a>
-      <a
-        className="m-4 p-4 border-2 rounded-md  text-white text-2xl"
+        className="m-4 p-4 border-2 rounded-md  text-white text-2xl hover:bg-gray-400 transition-all 0.3s ease-in hover:text-green-900"
         href="mailto:oliviervo.pro@gmail.com?"
         target="blank"
       >
         <FontAwesomeIcon icon={faMailBulk} />
       </a>
       <a
-        className="m-4 p-4 border-2 rounded-md  text-white text-2xl"
+        className="m-4 p-4 border-2 rounded-md  text-white text-2xl hover:bg-gray-400 transition-all 0.3s ease-in hover:text-green-900"
         target="blank"
-        href="/assets/CV Junior - Bac+2.pdf"
-        download="/assets/CV Junior - Bac+2.pdf"
+        href="/assets/CV-Vo-Olivier.pdf"
+        download="/assets/CV-Vo-Olivier.pdf"
       >
         <FontAwesomeIcon icon={faFile} />: C.V.
       </a>
